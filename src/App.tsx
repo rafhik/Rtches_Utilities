a29554b8b73510a112beff79c302a3ecc6ec970d
Rafael <rtches@gmail.com>
2026-04-03 13:39:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -38,33 +38,6 @@ function App() {
         <SectionPlanesCreator></SectionPlanesCreator>
       </section>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
       <section id="spacer"></section>
     </>
   )
