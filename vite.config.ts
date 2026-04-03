ce28d88c96d6d5d837e3b37f1c5be8bb47e0bd9e
Rafael <rtches@gmail.com>
2026-04-03 10:51:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/Rtches_Utilities/'
+    base: '/Rtches_Utilities/',
     plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
